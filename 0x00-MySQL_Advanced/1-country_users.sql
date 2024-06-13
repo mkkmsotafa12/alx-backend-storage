3d894d1a4597b92cdfe49dad927713f0d3d9564c
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-06-13 16:09:55 -0400

    1. In and not out

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,7 @@
--- Task: Create a table named `users`
+-- A SQL script that creates a table users
+DROP TABLE IF EXISTS users;
 
+-- Create the users 'table' with specified attributes
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT,
 
