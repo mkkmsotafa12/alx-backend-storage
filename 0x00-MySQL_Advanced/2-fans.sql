58faf9ad014c1c9e8886dc627c8da18cc6161365
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-06-13 16:10:33 -0400

    2. Best band ever!

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,4 @@
---  script that ranks country origins of bands,
+-- A SQL script that ranks country origins of bands,
 -- ordered by the number of (non-unique) fans
 
 SELECT origin, SUM(fans) AS nb_fans FROM metal_bands
