d34de7ae73bc16008c58dac758b9bb85d148bf3c
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-06-13 08:39:01 -0400

    1. In and not out

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,10 @@
+-- Task: Create a table named `users`
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255),
+  country NOT NULL ENUM('US', 'CO', 'TN') DEFAULT 'US'
+);
