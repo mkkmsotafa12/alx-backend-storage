d9c29afbcbd6e0b5b381921c76d7394e850a51ec
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-06-13 08:33:54 -0400

    0. We are all unique!

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- Task: Create a table named `users`
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255)
+);
