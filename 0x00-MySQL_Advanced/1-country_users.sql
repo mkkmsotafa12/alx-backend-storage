bf5536d7cb7b7b2003f5789ef48768cd5f3a97bc
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-06-13 08:42:11 -0400

    1. In and not out

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,22 @@
 -- Task: Create a table named `users`
-DROP TABLE IF EXISTS users;
 
-CREATE TABLE users (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  name VARCHAR(255),
-  country NOT NULL ENUM('US', 'CO', 'TN') DEFAULT 'US'
+CREATE TABLE IF NOT EXISTS users (
+    -- `id` is an integer that auto increments and serves as the primary key
+    id INT AUTO_INCREMENT,
+
+    -- `email` is a string of up to 255 characters that must be unique and not null
+    email VARCHAR(255) NOT NULL,
+
+    -- `name` is a string of up to 255 characters
+    name VARCHAR(255),
+
+    -- `country` is an enumeration of countries: US, CO and TN, never null
+    -- default will be the first element of the enumeration, here US
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+
+    -- Define `id` as the primary key
+    PRIMARY KEY (id),
+
+    -- Ensure that `email` is unique across all records
+    UNIQUE (email)
 );
